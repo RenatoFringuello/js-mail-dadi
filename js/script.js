dcb5a1c749ed54c78adb1c914522d31726d9cfcb
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-24 15:37:21 +0100

    add mail feature

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
+// gioco dei dadi
+        // creo un array con i valori del dado
 const dado = [1,2,3,4,5,6];
 
-let user, com, result;
+let user, com, result, nMatch = 10;
 
-for (let i = 0; i < 100; i++) {
+    // crea 10 match
+for (let i = 0; i < nMatch; i++) {
+    // genero una giocata random per ogni giocatore
     user = Math.floor(Math.random() * dado.length);
     com = Math.floor(Math.random() * dado.length);
     
+    // verifico il vincitore o il pareggio
     if(dado[user] > dado[com]){
         result = 'user win ';
     }else if(dado[user] < dado[com]){
@@ -13,5 +18,46 @@ for (let i = 0; i < 100; i++) {
     }else{
         result = 'equal ';
     }
+    // output dell'esito
     console.log(result, dado[user], dado[com]);
 }
+
+// mail check
+    // creo una lista di utenti verificati
+const users = [
+    'gino@gmail.com',
+    'pippo@gmail.com',
+    'paolina@gmail.com',
+    'andrea@gmail.com',
+    'maria@gmail.com'
+];
+    // recupero dal DOM gli elementi che mi servono
+        //userEMail per recuperare l'input utente
+const userEMail = document.getElementById('useremail');
+        //checkEMail per svolgere una funzione su un evento click del button
+const checkEMail = document.querySelector('.form button');
+        //response per recuperare l'elemento su cui inserire l'esito
+const response = document.getElementById('response');
+
+checkEMail.addEventListener('click', function(){
+    // riciclo le variabili result e user che tanto non ha più uno scopo
+    result = "utente non trovato";
+    user = users.length;//cosi se non trova nessuno restituisce 'undefined'
+
+    // giro tante volte quante sono le email in lista
+    for (let i = 0; i < users.length; i++) {
+        // verifico che la mail inserita esista fra quelle in lista
+        if(userEMail.value === users[i]){
+            // tengo conto della posizione
+            result = "Bentornato";
+            user = i;
+        }
+        /**
+         *  else non faccio nulla in quanto di default resul ha un messaggio con
+         *  esito negativo e la posizione in 'user' non tiene conto di nessun elemento in lista
+         */
+    }
+    // metto un output dell'esito, della mail inserita, e di quella corrispondente in lista
+    response.innerHTML = (users[user] !== undefined) ? `${result} ${users[user]} ci sono 2345 email da leggere` : result;
+    console.log(result, userEMail.value, users[user]);
+});
